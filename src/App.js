e033141302532837bd500201bacb93ae3c14eec4
HetRojivadiya <hetrojivadiya999@gmail.com>
2023-08-02 21:58:00 +0530

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+const TodoList = () => {
+  const [list, setList] = useState([]);
+  const [valueTodo, setValue] = useState("");
+  const [editMode, setEditMode] = useState(null);
+  const [editText, setEditText] = useState("");
+
+  const changeValue = (event) => {
+    setValue(event.target.value);
+  };
+
+  const addList = () => {
+    if (valueTodo.trim() !== "") {
+      const newTodo = {
+        id: Date.now(),
+        text: valueTodo,
+      };
+      setList([...list, newTodo]);
+      setValue("");
+    }
+  };
+
+  const editTodo = (id, text) => {
+    setEditMode(id);
+    setEditText(text);
+  };
+
+  const changeEdit = (e) => {
+    setEditText(e.target.value);
+  };
+
+  const saveTodo = (id) => {
+    if (editText.trim() !== "") {
+      setList(
+        list.map((todo) => (todo.id === id ? { ...todo, text:editText } : todo))
+      );
+      setEditMode(null);
+      setEditText("");
+    }
+  };
+
+  const deleteTodo = (id) => {
+    const newList = list.filter((todo) => todo.id !== id);
+    setList(newList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container my-3">
+        <h1>Todo List</h1>
+        <div className="center">
+          <input
+            type="text"
+            id="value"
+            onChange={changeValue}
+            value={valueTodo}
+            placeholder="Add a new task..."
+          />
+          <button id="add-task" onClick={addList} className="text-center">
+            Add
+          </button>
+        </div>
+        <ul>
+          {list.map((todo) => (
+            <li key={todo.id}>
+              
+              {editMode === todo.id ? (
+                <input
+                  type="text"
+                  value={editText}
+                  onChange={changeEdit}
+                />
+              ) : (
+                todo.text
+              )}
+              {editMode === todo.id ? (
+                <>
+                  <button className="save" onClick={() => saveTodo(todo.id)}>Save</button>
+                  <button className="cancel" onClick={() => setEditMode(null)}>Cancel</button>
+                </>
+              ) : (
+                <>
+                  
+                  <button className="edit" onClick={() => editTodo(todo.id, todo.text)}>Edit</button>
+                  <button className="delete" onClick={() => deleteTodo(todo.id)}>Delete</button>
+                </>
+              )}
+            </li>
+          ))}
+          <li>Total Task : {list.length}</li>
+        </ul>
+      </div>
+    </>
   );
-}
+};
 
-export default App;
+export default TodoList;
